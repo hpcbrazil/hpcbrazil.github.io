9d51a1d286173b7ad57a522e52dc754cd0bf0ce7
hpcbrazil <brazilhpc@gmail.com>
2025-02-28 15:05:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,13 @@ var locations = [
     { name: "LNCC - Santos Dumont", coords: [-22.5045, -43.1785] },
     { name: "SENAI CIMATEC", coords: [-12.8654, -38.4322] },
     { name: "CESUP/UFRGS", coords: [-30.0331, -51.2302] },
-    { name: "NACAD/UFRJ", coords: [-22.8600, -43.2226] },
-    { name: "INPE", coords: [-23.2083, -45.8723] }
+    { name: "CENAPAD-SP", coords: [-22.8142, -47.0647] },
+    { name: "CENAPAD-RJ", coords: [-22.8600, -43.2226] },
+    { name: "CENAPAD-MG", coords: [-19.8700, -43.9678] },
+    { name: "CENAPAD-CPTEC/INPE", coords: [-23.2083, -45.8723] },
+    { name: "CENAPAD-UFC", coords: [-3.745, -38.574] },
+    { name: "CENAPAD-UFPE", coords: [-8.0500, -34.9500] },
+    { name: "CENAPAD-INPA", coords: [-3.1072, -60.0261] }
 ];
 
 // Adiciona marcadores para cada centro de HPC no mapa
